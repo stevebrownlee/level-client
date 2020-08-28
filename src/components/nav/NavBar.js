a96c526f266f9e98c3ea885e6c80377e4c1bbc51
Steve Brownlee <chortlehoort@gmail.com>
2020-08-28 08:13:51 -0500

    Initial commit

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+
+            </li>
+        </ul>
+    )
+}
